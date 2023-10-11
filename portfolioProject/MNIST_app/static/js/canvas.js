eb25ef2704e88fb4cf56187cbb89c53a65d3ccde
fredriknystrom <fredriknystroms@gmail.com>
2023-10-12 00:31:59 +0200

    fixed csrf token and static js files

---
+++ portfolioProject/MNIST_app/static/js/canvas.js
@@ -0,0 +1,121 @@
+const canvas = document.getElementById('drawingCanvas');
+const context = canvas.getContext('2d');
+let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+let drawing = false;
+
+canvas.addEventListener('mousedown', () => {
+    drawing = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+    context.beginPath();
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (!drawing) return;
+    draw(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+});
+
+function draw(x, y) {
+    context.lineWidth = 30;
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.lineTo(x, y);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(x, y);
+}
+
+function convertDataToMatrix(imageData) {
+    const matrix = [];
+
+    // Loop through the image data and create a 2D matrix
+    for (let y = 0; y < imageData.height; y++) {
+        const row = [];
+        for (let x = 0; x < imageData.width; x++) {
+            const index = (y * imageData.width + x) * 4;
+            const pixelValue = imageData.data[index+3]; // this is used dont care about other
+            const g = imageData.data[index + 1];
+            const b = imageData.data[index + 2];
+
+            // Apply sRGB gamma correction
+            //const gammaCorrectedR = Math.pow(r / 255, 2.2);
+            const gammaCorrectedG = Math.pow(g / 255, 2.2);
+            const gammaCorrectedB = Math.pow(b / 255, 2.2);
+
+            // Calculate grayscale value using gamma-corrected values
+            //const grayscale = (gammaCorrectedR + gammaCorrectedG + gammaCorrectedB) / 3;
+
+            row.push(pixelValue);
+        }
+        matrix.push(row);
+    }
+
+    return matrix;
+}
+
+function sendToServer(matrixData) {
+    // Get the CSRF token from the page's cookie
+    const csrfToken = getCookie('csrftoken');
+
+    // Convert the matrix data to JSON
+    const jsonData = JSON.stringify({ pixel_data: matrixData });
+
+    // Make a POST request to your Django server with the CSRF token included
+    fetch('/mnist/', {
+        method: 'POST',
+        body: jsonData,
+        headers: {
+            'X-CSRFToken': csrfToken, // Include the CSRF token in the headers
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const predictionResult = document.getElementById('predictionResult');
+        predictionResult.innerHTML = `Predicted Number: ${data.prediction}`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Function to get the CSRF token from cookies
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.startsWith(name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const guessButton = document.getElementById('guessButton');
+guessButton.addEventListener('click', () => {
+    // Log the grayscale data
+    console.log(imageData);
+
+    data_matrix = convertDataToMatrix(imageData)
+
+    console.log(data_matrix);
+
+    // Send the grayscale data to the server
+    sendToServer(data_matrix);
+});
+
+const clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', () => {
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // Reset the image data to a blank image
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+});
--- portfolioProject/MNIST_app/views.py
+++ portfolioProject/MNIST_app/views.py
@@ -15,26 +15,25 @@ filename = 'mnist_model.keras'
 model_path = os.path.join(settings.BASE_DIR, 'MNIST_app', 'ml_models', filename)
 model = tf.keras.models.load_model(model_path)
 
-@csrf_exempt
+
 def mnist_view(request):
     if request.method == 'POST':
-        #try:
-            # Get the JSON data from the request
-            data = json.loads(request.body)
-         
-            # Extract the pixel data and convert it to a NumPy array
-            pixel_data = np.array(data.get('pixel_data', []))
-          
-            # Perform any required preprocessing (e.g., cropping)
-            preprocessed_data = cv2_crop(pixel_data)
-            
-            # Make predictions using your model
-            predictions = model.predict(np.expand_dims(preprocessed_data, axis=0))
+        # Get the JSON data from the request
+        data = json.loads(request.body)
 
-            # Convert predictions to a format you want to return to the frontend
-            predicted_number = np.argmax(predictions[0])  # Assuming it's a classification task
+        # Extract the pixel data and convert it to a NumPy array
+        pixel_data = np.array(data.get('pixel_data', []))
 
-            # Return the predicted result as JSON
-            return JsonResponse({'prediction': int(predicted_number)})
+        # Perform any required preprocessing (e.g., cropping)
+        preprocessed_data = cv2_crop(pixel_data)
+
+        # Make predictions using your model
+        predictions = model.predict(np.expand_dims(preprocessed_data, axis=0))
+
+        # Convert predictions to a format you want to return to the frontend
+        predicted_number = np.argmax(predictions[0])  # Assuming it's a classification task
+
+        # Return the predicted result as JSON
+        return JsonResponse({'prediction': int(predicted_number)})
     else:
         return render(request, 'MNIST_app/canvas.html')  # Replace 'mnist.html' with your actual HTML template
