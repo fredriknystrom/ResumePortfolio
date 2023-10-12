d9961d84ac2dc9532cee5efdc311c778116791e7
fredriknystrom <fredriknystroms@gmail.com>
2023-10-12 21:51:14 +0200

    fixed favicons

--- portfolioProject/portfolioProject/urls.py
+++ portfolioProject/portfolioProject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('mnist/', include('MNIST_app.urls')),
     path('spotistats/', include('spotistats_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
