cc3310d123f47a55b1f762f54077e02e414c5769
fredriknystrom <fredriknystroms@gmail.com>
2023-10-16 23:23:06 +0200

    added some animations

--- portfolioProject/resume_app/static/js/resume.js
+++ portfolioProject/resume_app/static/js/resume.js
@@ -14,10 +14,28 @@ $(document).ready(function() {
             // After navbar is collapsed, smoothly scroll to the target section
             $('#navbarNav').on('hidden.bs.collapse', function() {
                 $('html, body').animate({
-                    scrollTop: $(targetSection).offset().top - 105
+                    scrollTop: $(targetSection).offset().top
                 }, 20); // 20 ms animation speed
             });
         }
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const typeString = 'Hello, my name is Fredrik!';
+    let i = 0;
+    console.log(typeString)
+    const typeWriter = () => {
+        if (i < typeString.length) {
+            document.querySelector('.hello-header').textContent += typeString.charAt(i);
+            i++;
+            setTimeout(typeWriter, 100);  // Adjust the typing speed by changing the timeout value.
+        } else {
+            // Remove the typing class to stop the cursor from blinking after typing is complete.
+            document.querySelector('.hello-header').classList.remove('typing');
+        }
+    };
+
+    // Start the typewriter effect
+    typeWriter();
+});
