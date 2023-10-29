405521d37d3eedc0cc37c609f39a911f2323327b
fredriknystrom <fredriknystroms@gmail.com>
2023-10-29 12:08:20 +0100

    started working on math notes

--- portfolioProject/private_app/urls.py
+++ portfolioProject/private_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from private_app import views
 
 urlpatterns = [
-    path('', views.terminal_view, name='terminal'),
+    path('', views.private_view, name='private'),
+    path('terminal', views.terminal_view, name='terminal'),
+    path('math', views.math_view, name='math'),
 ]
--- portfolioProject/private_app/views.py
+++ portfolioProject/private_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def private_view(request):
+    return render(request, 'private_app/base.html')
+
 def terminal_view(request):
     return render(request, 'private_app/terminal.html')
+
+def math_view(request):
+    return render(request, 'private_app/math.html')
