ccb3c01185a3dade11ed5f246718b8f79e0a4b46
fredriknystrom <fredriknystroms@gmail.com>
2024-03-13 13:51:14 +0100

    darkmode slider working for desktop

--- portfolioProject/resume_app/static/js/toogleColorMode.js
+++ portfolioProject/resume_app/static/js/toogleColorMode.js
@@ -1,16 +1,6 @@
 
-document.getElementById('themeToggle').addEventListener('click', function() {
-    const bodyElement = document.body;
-    const themeIcon = document.getElementById('themeToggleIcon'); // Make sure this ID matches your icon element
-
-    // Toggle the 'light-mode' class on the body
-    bodyElement.classList.toggle('light-mode');
-
-    // Update the icon to reflect the change
-    if (bodyElement.classList.contains('light-mode')) {
-        themeIcon.className = 'fa fa-tint'; // Change to your light mode icon class
-    } else {
-        themeIcon.className = 'fa fa-lightbulb-o'; // Change to your default mode icon class
-    }
-    event.preventDefault();
+document.getElementById('darkModeToggle').addEventListener('change', function(event) {
+    document.body.classList.toggle('dark-mode');
+    // Optionally save the state in localStorage or a cookie
 });
+
