819cd21fc46bfbab88b7170db0baeee656fa91ac
fredriknystrom <fredriknystroms@gmail.com>
2024-05-30 20:21:40 +0200

    updated model with date and priority

--- portfolioProject/todolist_app/models.py
+++ portfolioProject/todolist_app/models.py
@@ -1,11 +1,27 @@
-from django.db import models
 import uuid
+from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    class Priority(models.TextChoices):
+        HIGH = 'high', 'High'
+        MEDIUM = 'medium', 'Medium'
+        LOW = 'low', 'Low'
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.MEDIUM)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.user = kwargs.pop('user', None)
+        super().save(*args, **kwargs)
+
+
+   
