b0612e048a65dc756b81eaf9d7e289c417e09516
fredriknystrom <fredriknystroms@gmail.com>
2024-05-30 22:01:03 +0200

    changed priority to status and added done

--- portfolioProject/todolist_app/models.py
+++ portfolioProject/todolist_app/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    class Priority(models.TextChoices):
-        HIGH = 'high', 'High'
-        MEDIUM = 'medium', 'Medium'
-        LOW = 'low', 'Low'
+    class Status(models.TextChoices):
+        HIGH = '1', 'High'
+        MEDIUM = '2', 'Medium'
+        LOW = '3', 'Low'
+        Done = '4', 'Done'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.MEDIUM)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.MEDIUM)
 
     def __str__(self):
         return self.title
