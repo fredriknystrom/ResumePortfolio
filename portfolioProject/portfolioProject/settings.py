5ddbaa59003cbf2efeb30d6e6385b0db4e20ecdc
fredriknystrom <fredriknystroms@gmail.com>
2024-05-31 19:59:59 +0200

    added google login

--- portfolioProject/portfolioProject/settings.py
+++ portfolioProject/portfolioProject/settings.py
@@ -82,22 +82,6 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE' : [
-            'profile',
-            'email'
-        ],
-        'APP': {
-            'client_id': os.getenv('CLIENT_ID'),
-            'secret': os.getenv('CLIENT_SECRET'),
-        },
-        'AUTH_PARAMS': {
-            'access_type':'online',
-        }
-    }
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
