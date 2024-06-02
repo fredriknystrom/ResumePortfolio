89f37eaf4eede430d08e5ec2420676b92bc61779
fredriknystrom <fredriknystroms@gmail.com>
2024-06-03 00:42:42 +0200

    overriding allauth socialacount login template

--- portfolioProject/portfolioProject/settings.py
+++ portfolioProject/portfolioProject/settings.py
@@ -169,6 +169,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# Static file for overriding oauth login templates
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
